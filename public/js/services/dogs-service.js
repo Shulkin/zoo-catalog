d08926bd35f7b5f169453c72495dd118f27725c9
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2016-10-26 11:06:50 +1000

    Implemented dogs service

--- public/js/services/dogs-service.js
+++ public/js/services/dogs-service.js
@@ -1,3 +1,20 @@
 angular.module("dogs.service", [])
 .factory("Dogs", function($http) {
+  return {
+    getAll: function() {
+      return $http.get("/api/dogs");
+    },
+    get: function(id) {
+      return $http.get("/api/dogs/" + id);
+    },
+    create: function(dogData) {
+      return $http.post("/api/dogs", dogData);
+    },
+    update: function(id, dogData) {
+      return $http.put("/api/dogs/" + id, dogData);
+    },
+    delete: function(id) {
+      return $http.delete("/api/dogs/" + id);
+    }
+  }
 });
