d903989099106b73413cc7ede0f5c64282fabf1c
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2016-10-28 14:09:03 +1000

    Added page navigation in gallery

--- public/js/controllers/dogs-ctrl.js
+++ public/js/controllers/dogs-ctrl.js
@@ -1,39 +1,46 @@
 // we have access to Dogs factory through main module
 angular.module("dogs.ctrl", [])
 .controller("DogsCtrl", function(Dogs, DogsMock, ModalService, Utils) {
-  /*
-   * Better use 'this' instead of $scope, for several reasons.
-   * But you need to declare 'this' as variable first!
-   */
-  var vm = this; // view model
-  // when landing on page, load all dogs and show them
-  Dogs.getAll()
-  .success(function(data) {
-    /*
-     * Important notice!
-     * Use of vm here insted of 'this' is mandatory! 'this' here is not the
-     * same as in DogsCtrl main body.
-     */
-    data = DogsMock.createList(100); // create test mock list with many random dogs
-    // populate dogs list
-    vm.list = data;
-    // divide list to pages, up to 11 dogs on one page
-    vm.pages = Utils.split(data.slice(), 11); // use slice() to keep original data intact
-  })
-  .error(function(err) {
-    console.log("Error " + err);
-  });
+  // === Private ===
+  // split to pages
+  function splitPages(data) {
+    // up to 11 dogs on one page
+    return Utils.split(data, 11);
+  }
   // create new dog
-  // keep it as a private function inside controller
   function create(data) {
     Dogs.create(data)
     .success(function(data) {
-      vm.list = data; // update list with new dog
+      /*
+       * Important notice!
+       * Use of vm here insted of 'this' is mandatory! 'this' here is not the
+       * same as in DogsCtrl main body.
+       */
+      vm.pages = splitPages(data);
     })
     .error(function(err) {
       console.log("Error " + err);
     });
   };
+  /*
+   * Better use 'this' instead of $scope, for several reasons.
+   * But you need to declare 'this' as variable first!
+   */
+  var vm = this; // view model
+  // === Constructor ===
+  Dogs.getAll()
+  .success(function(data) {
+    // create test mock list with many random dogs
+    data = DogsMock.createList(100);
+    // divide dogs list to pages, up to 11 dogs on one page
+    vm.pages = splitPages(data);
+    // set first page by default
+    vm.changePage(0);
+  })
+  .error(function(err) {
+    console.log("Error " + err);
+  });
+  // === Public ===
   // delete a dog
   vm.delete = function(id) {
     Dogs.delete(id)
@@ -44,9 +51,9 @@ angular.module("dogs.ctrl", [])
       console.log("Error " + err);
     });
   };
-  // show modal dialog
+  // show create dog dialog
   vm.openDialog = function() {
-    // use third-party service to open simple modal dialog
+    // use third-party service to open modal
     ModalService.showModal({
       templateUrl: "../views/create-dialog.html",
       controller: "ModalCtrl as dialog",
@@ -57,10 +64,15 @@ angular.module("dogs.ctrl", [])
       modal.element.modal();
       modal.close.then(function(result) {
         // result.data is an object with new dogs params
-        if (result.success === true) { // if not canceled dialog
+        if (result.success === true) { // if dialog not cancel
           create(result.data);
         }
       });
     });
+  };
+  // change page in gallery
+  vm.changePage = function(index) {
+    vm.pageIndex = index; // selected page
+    vm.currentPage = vm.pages[index];
   };
 });
