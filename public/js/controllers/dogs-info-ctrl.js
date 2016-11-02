14161da162b980c9ecd83de7da0b2c2ee489e52d
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2016-11-02 15:18:51 +1000

    Added image placeholder and description to dogs info

--- public/js/controllers/dogs-info-ctrl.js
+++ public/js/controllers/dogs-info-ctrl.js
@@ -1,8 +1,11 @@
 angular.module("dogs.info.ctrl", [])
 .controller("DogsInfoCtrl", function(Dogs, $stateParams) {
   var vm = this;
+  // init params with empty string
+  [vm.name, vm.age, vm.breed] = ["","",""]
   // receive dogs id from url
   var id = $stateParams.id;
+  // find dog by id and fill params
   Dogs.get(id)
   .success(function(dog) {
     vm.name = dog.name;
