5bcd7dadc9436c7b54482d0a17ba341e45a0e83e
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2016-11-03 15:25:29 +1000

    Added tools to edit dogs params in card

--- public/js/controllers/dogs-info-ctrl.js
+++ public/js/controllers/dogs-info-ctrl.js
@@ -3,6 +3,9 @@ angular.module("dogs.info.ctrl", [])
   var vm = this;
   // init params with empty string
   [vm.name, vm.age, vm.breed] = ["", "", ""]
+  // default edit flags for inputs
+  vm.edit = {};
+  [vm.edit.name, vm.edit.age, vm.edit.breed] = [false, false, false];
   // receive dogs id from url
   var id = $stateParams.id;
   // find dog by id and fill params
@@ -15,4 +18,28 @@ angular.module("dogs.info.ctrl", [])
   .error(function(err) {
     console.log("Error " + err);
   });
+  // === Public ===
+  vm.startEdit = function(val) {
+    // turn on input
+    vm.edit[val] = true;
+  };
+  vm.submit = function(val) {
+    // submit all data
+    var data = {
+      name: vm.name,
+      age: vm.age,
+      breed: vm.breed
+    };
+    Dogs.update(id, data)
+    .success(function(dog) {
+      vm.name = dog.name;
+      vm.age = dog.age;
+      vm.breed = dog.breed;
+    })
+    .error(function(err) {
+      console.log("Error " + err);
+    });
+    // turn off input
+    vm.edit[val] = false;
+  };
 });
